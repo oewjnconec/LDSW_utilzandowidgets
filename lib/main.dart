f47650527ecc7604d87235c4f46676a7bd00ffa4
Diego Chavez <defraindefrain.chavez@alumnos.udg.mx>
2024-10-25 19:37:15 -0600

    Initial commit with movie catalog structure

--- lib/main.dart
+++ lib/main.dart
@@ -10,12 +10,12 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Welcome to Flutter',  
+      title: 'Movie Catalog',
       theme: ThemeData(
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
         useMaterial3: true,
       ),
-      home: const MyHomePage(title: 'Welcome to Flutter'),  
+      home: const MyHomePage(title: 'Movie Catalog'),
     );
   }
 }
@@ -30,43 +30,45 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
-
-  void _incrementCounter() {
-    setState(() {
-      _counter++;
-    });
-  }
+  final List<Map<String, String>> movies = [
+    {"title": "Movie 1", "description": "Description of movie 1"},
+    {"title": "Movie 2", "description": "Description of movie 2"},
+    {"title": "Movie 3", "description": "Description of movie 3"},
+  ];
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text(widget.title),  
+        title: Text(widget.title),
       ),
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            const Text(
-              'Hello World',  
-              style: TextStyle(fontSize: 24), 
+      body: ListView.builder(
+        itemCount: movies.length,
+        itemBuilder: (context, index) {
+          return Container(
+            margin: const EdgeInsets.all(10),
+            padding: const EdgeInsets.all(10),
+            decoration: BoxDecoration(
+              color: Colors.deepPurple[50],
+              border: Border.all(color: Colors.deepPurple),
+              borderRadius: BorderRadius.circular(10),
             ),
-            const SizedBox(height: 20), 
-            const Text(
-              'You have pushed the button this many times:',
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                Text(
+                  movies[index]['title']!,
+                  style: const TextStyle(
+                    fontSize: 20,
+                    fontWeight: FontWeight.bold,
+                  ),
+                ),
+                const SizedBox(height: 5),
+                Text(movies[index]['description']!),
+              ],
             ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headlineMedium,
-            ),
-          ],
-        ),
-      ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
+          );
+        },
       ),
     );
   }
